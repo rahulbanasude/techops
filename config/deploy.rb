cfa7b4b0126819dc5d01974159df0b9f1d1a0d87
rahulbanasude <rahul.bansude2@gmail.com>
2017-01-13 01:35:19 +0530

    fix 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,5 +32,17 @@ set :deploy_to, '/home/deploy/techops'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
