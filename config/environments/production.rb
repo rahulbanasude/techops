c49b2570f04ffb61a35ddb5f07e6c17175d3db54
rahulbanasude <rahul.bansude2@gmail.com>
2017-01-13 23:17:56 +0530

    clean coming soon page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,13 +12,7 @@ Rails.application.configure do
 config.serve_static_assets = false
 
 # Compress JavaScripts and CSS  
-config.assets.compress = true
 
-# Don't fallback to assets pipeline if a precompiled asset is missed  
-config.assets.compile = false
-
-# Generate digests for assets URLs  
-config.assets.digest = true
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
