6ab918f46589864b72c23ac11e451fccb397b161
rahulbanasude <rahul.bansude2@gmail.com>
2017-02-16 11:50:06 +0530

    added logo

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'devise'
 gem 'cancan' # or cancancan
 gem 'draper'
 gem 'pundit'
+gem 'carrierwave', '~> 1.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,23 @@
-class RegistrationsController < InheritedResources::Base
+class RegistrationsController < ApplicationController
 
+
+  def registration
+    @registration = Registration.new
+  end
+
+  def create
+    @registration = Registration.new(registration_params)
+  if @registration.save
+    redirect_to root_path, notice: "Thank you for Registering"
+  else
+    render 'new', alert: "Registration failed."
+
+  end
+end
   private
 
     def registration_params
       params.require(:registration).permit(:name, :email, :number, :college)
     end
-end
 
+end
